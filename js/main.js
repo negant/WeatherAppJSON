2476adae58e1835381cd6fcbf001cc806ff3cec5
marcinszymanowski <marcinszymanowski1984@gmail.com>
2017-05-15 21:18:51 +0200

    js mod

--- js/main.js
+++ js/main.js
@@ -24,15 +24,8 @@ weatherConditions.onload = function() {
 
 
 
-
-
-
-
-
-
-
 // GET THE FORECARST
-weatherForecast.open('GET', 'http://api.wunderground.com/api/9575659d2a2567a0/conditions/q/84653.json', true);
+weatherForecast.open('GET', 'http://api.wunderground.com/api/9575659d2a2567a0/forecast/q/84653.json', true);
 weatherForecast.responseType = 'text'; 
 weatherForecast.send();
 
@@ -40,7 +33,25 @@ weatherForecast.onload = function() {
 if (weatherForecast.status === 200){
 	fObj = JSON.parse(weatherForecast.responseText);
 	console.log(fObj);
-	
+	document.getElementById('desc').innerHTML = fObj.forecast.txt_forecast.forecastday[0].fcttext;
+    // Day 1
+	document.getElementById('r1c1').innerHTML = fObj.forecast.simpleforecast.forecastday[1].date.weekday;
+	document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.celsius + "°";
+	document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.celsius + "°";
+    var imagePath = fObj.forecast.simpleforecast.forecastday[1].icon_url;
+    document.getElementById('r1c2').src = imagePath;
+    // Day 2
+	document.getElementById('r2c1').innerHTML = fObj.forecast.simpleforecast.forecastday[2].date.weekday;
+	document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.celsius + "°";
+	document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.celsius + "°";
+    var imagePath = fObj.forecast.simpleforecast.forecastday[2].icon_url;
+    document.getElementById('r2c2').src = imagePath;
+    // Day 3
+	document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
+	document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.celsius + "°";
+	document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.celsius + "°";
+    var imagePath = fObj.forecast.simpleforecast.forecastday[3].icon_url;
+    document.getElementById('r3c2').src = imagePath;
 } //end if
 }; //end function
 
