2418ecfacb41b05c850a85e2d268bb69ef6f3c69
marcinszymanowski <marcinszymanowski1984@gmail.com>
2017-05-15 20:44:15 +0200

    WeatherApp JSON

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*jslint browser:true */
+'use strict';
+
+var weatherConditions = new XMLHttpRequest();
+var weatherForecast = new XMLHttpRequest();
+var cObj;
+var fObj;
+
+// GET THE CONDITIONS
+weatherConditions.open('GET', 'http://api.wunderground.com/api/9575659d2a2567a0/conditions/q/84653.json', true);
+weatherConditions.responseType = 'text';
+weatherConditions.send(null);
+
+weatherConditions.onload = function() {
+    if (weatherConditions.status === 200){
+        cObj = JSON.parse(weatherConditions.responseText); 
+        console.log(cObj);
+        document.getElementById('location').innerHTML = cObj.current_observation.display_location.full;
+        document.getElementById('weather').innerHTML = cObj.current_observation.weather;
+        document.getElementById('temperature').innerHTML = cObj.current_observation.temp_c;
+
+    } //end if
+}; //end function
+
+
+
+
+
+
+
+
+
+
+// GET THE FORECARST
+weatherForecast.open('GET', 'http://api.wunderground.com/api/9575659d2a2567a0/conditions/q/84653.json', true);
+weatherForecast.responseType = 'text'; 
+weatherForecast.send();
+
+weatherForecast.onload = function() {
+if (weatherForecast.status === 200){
+	fObj = JSON.parse(weatherForecast.responseText);
+	console.log(fObj);
+	
+} //end if
+}; //end function
+
+
